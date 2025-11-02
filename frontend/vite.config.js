86706784db5c394efb6dc580fdd8fa0402ab3a8d
adil112211 <adilrgbk@gmail.com>
2025-11-03 04:14:06 +0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -7,12 +6,5 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    // Прокси для обхода CORS в режиме разработки
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      },
-    },
   },
 })
